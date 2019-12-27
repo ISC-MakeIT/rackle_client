d29ff0268cce4dc0ada7361ed03e92af99469e4a
isshun226 <bjsi1290346@gn.iwasaki.ac.jp>
2019-12-27 23:29:55 +0900

    write themeData-template

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
+import './theme.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      title: 'らっくる',
+      theme: rackleTheme, 
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }

98c41b0cf4148085b551656121a49565e7e58e59
isshun226 <bjsi1290346@gn.iwasaki.ac.jp>
2019-12-30 20:57:32 +0900

    Fixed icon color

--- lib/widgets/BottomNav/BottomMenuBar.dart
+++ lib/widgets/BottomNav/BottomMenuBar.dart
@@ -6,22 +6,32 @@ class BottomMenuBar extends StatelessWidget {
     return BottomNavigationBar(
       items: [
         BottomNavigationBarItem(
-          icon: Icon(Icons.map),
+          icon: Icon(Icons.map,
+            color: Color(0xFFB3BAAB),
+          ),
           title: Text('マップ'),
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.pin_drop),
+          icon: Icon(Icons.pin_drop,
+            color: Color(0xFFB3BAAB),
+          ),
           title: Text('ピン一覧'),
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.notifications),
+          icon: Icon(Icons.notifications,
+            color: Color(0xFFB3BAAB),
+          ),
           title: Text('お知らせ'),
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.settings),
+          icon: Icon(Icons.settings,
+            color: Color(0xFFB3BAAB),
+          ),
           title: Text('設定'),
         ),
       ],
+      currentIndex: 1,
+      fixedColor: Colors.white,
     );
   }
 }

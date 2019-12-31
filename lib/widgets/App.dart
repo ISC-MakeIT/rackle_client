b18b305e2a14d5b06ef8deef4e4495dc7fa4bc07
isshun226 <bjsi1290346@gn.iwasaki.ac.jp>
2019-12-31 15:26:55 +0900

    Added tmp ThemeData

--- lib/widgets/App.dart
+++ lib/widgets/App.dart
@@ -8,6 +8,19 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'らっくる',
+      theme: ThemeData(
+        textTheme: TextTheme(
+          button: TextStyle(
+            color: Color(0xFF53514B),
+          ),
+        ),
+        iconTheme: IconThemeData(
+          color: Color(0xFFB3BAAB),          
+        ),
+        accentIconTheme: IconThemeData(
+          color: Color(0xFF53514B),
+        ),
+      ),
       home: Scaffold(
         body: Container(),
         bottomNavigationBar: BottomMenuBar(),

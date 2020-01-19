71463a8b3c32df3babc044ab1e763d9f71abd4bc
isshun226 <bjsi1290346@gn.iwasaki.ac.jp>
2020-01-20 00:57:42 +0900

    Write routes for navigation

---
+++ lib/App/routes/routes.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Map<String, Widget> routes = {
+	'/map' : MapViews(),
+	'/pinList' : PinListViews(),
+	'/notifications' : NotificationsViews(),
+	'/configuration' : ConfigurationViews(),
+};
+

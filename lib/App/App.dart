9abf4b4a373b30a02061bde84bb09472e0f991d3
tttkouyou <bjkt1290248@gn.iwasaki.ac.jp>
2020-01-21 18:20:26 +0900

    [ADD] splash screen  complete

--- lib/App/App.dart
+++ lib/App/App.dart
@@ -1,20 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 import '../theme/rackleTheme.dart';
 //import './routes/routes.dart';
 import './BottomNav/BottomMenuBar.dart';
 
-class App extends StatelessWidget {
+class Splashscreen extends StatefulWidget {
+  @override
+  SplashscreenState createState() => SplashscreenState();
+}
+
+class SplashscreenState extends State<Splashscreen> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'らっくる',
-      theme: rackleTheme, 
-			//routes: routes,
-      home: Scaffold(
-        body: Container(),
-        bottomNavigationBar: BottomMenuBar(),
-      ),
+      theme: rackleTheme,
+      home: SplashScreen(
+          seconds: 7,
+          navigateAfterSeconds: App(),
+          image: Image.asset(
+            'assets/logo_rackle.png',
+          ),
+          photoSize: 140.0,
+          imageBackground: AssetImage('assets/logo_background.png'),
+          loaderColor: Colors.white),
+    );
+  }
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return
+        // MaterialApp(
+        // title: 'らっくる',
+        // theme: rackleTheme,
+        //routes: routes,
+        // home:
+        Scaffold(
+      body: Container(),
+      bottomNavigationBar: BottomMenuBar(),
+      // ),
     );
   }
 }

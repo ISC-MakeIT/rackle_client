ec4c7090e6cfc1eca783f77ac8f62bff60191951
56miyano <bjgm1290561@gn.iwasaki.ac.jp>
2020-01-22 18:21:13 +0900

    add NotificationsPage.dart

---
+++ lib/App/NotificationsPage/NotificationsPage.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class NotificationsPage extends StatefulWidget {
+  @override
+  NotificationsPageState createState() => NotificationsPageState();
+}
+
+class NotificationsPageState extends NotificationsPageState<NotificationsPage> {
+}

8dc52ee6d5760adb6d36a7cf7bc0caf59915bcd5
tttkouyou <bjkt1290248@gn.iwasaki.ac.jp>
2020-01-24 20:00:51 +0900

    [ADD] Place widget on page

--- lib/App/pages/ConfigurationPage/ConfigurationPage.dart
+++ lib/App/pages/ConfigurationPage/ConfigurationPage.dart
@@ -1,10 +1,118 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class ConfigurationPage extends StatelessWidget {
+import './Heading.dart';
+import './PulldownList.dart';
+import './Birthday.dart';
+import './PostConfig.dart';
+import './SupportButtons.dart';
+
+class ConfigurationPage extends StatefulWidget {
+  @override
+  ConfigurationPageState createState() => ConfigurationPageState();
+}
+
+class ConfigurationPageState extends State<ConfigurationPage> {
+  final List<String> name = [
+    'ユーザー設定',
+    '性別',
+    '生年月日',
+    'ご利用環境',
+    '介護者',
+    'シニアモード',
+    'サポート',
+    'アプリのご利用方法',
+    'お問い合わせ',
+  ];
+  final List<String> sex = [
+    '男性',
+    '女性',
+    'その他',
+  ];
+  final List<String> used = [
+    '利用していない',
+    '車椅子利用',
+    '電動車椅子利用',
+    'スポーツ用車椅子',
+  ];
+  final List<String> caregiver = [
+    'なし',
+    'あり',
+  ];
+  final List<String> senior = [
+    'オフ',
+    'オン',
+  ];
+
+  final List<double> margin = [
+    44.0,
+    24.0,
+  ];
+
+  String userBirthday = '2000/1/1';
+
+  final formatter = DateFormat('yyyy/MM/dd');
+
+  setBirthday(item) {
+    setState(
+      () {
+        userBirthday = item;
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Configuration Page'),
+    return SingleChildScrollView(
+      child: Container(
+        child: Column(
+          children: <Widget>[
+            Heading(
+              name: name[0],
+              context: context,
+              margin: margin,
+            ),
+            PulldownList(
+              name: name[1],
+              list: sex,
+            ),
+            Birthday(
+              name: name[2],
+              formatter: formatter,
+              setBirthday: setBirthday,
+              userBirthday: userBirthday,
+            ),
+            PulldownList(
+              name: name[3],
+              list: used,
+            ),
+            PulldownList(
+              name: name[4],
+              list: caregiver,
+            ),
+            PulldownList(
+              name: name[5],
+              list: senior,
+            ),
+            PostConfig(
+              context: context,
+            ),
+            Heading(
+              name: name[6],
+              context: context,
+              margin: margin,
+            ),
+            SupportButtons(
+              name: name[7],
+              context: context,
+            ),
+            SupportButtons(
+              name: name[8],
+              context: context,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }

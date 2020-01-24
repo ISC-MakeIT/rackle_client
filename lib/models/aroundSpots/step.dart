bc3ef5f2a9204db11b10fe76257a1dd5f0682236
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2020-01-25 01:00:17 +0900

    :sparkles:ADD step class
    ~

---
+++ lib/models/aroundSpots/step.dart
@@ -0,0 +1,56 @@
+class Step {
+  final dynamic id;
+  final dynamic stationId;
+  final String imagePath;
+  final int floor;
+  final double lat;
+  final double lng;
+  final String name;
+  final String category;
+  final String description;
+  final bool level;
+  final String createdAt;
+  final String updatedAt;
+
+  Step(
+      {this.id,
+      this.stationId,
+      this.imagePath,
+      this.floor,
+      this.lat,
+      this.lng,
+      this.name,
+      this.category,
+      this.description,
+      this.level,
+      this.createdAt,
+      this.updatedAt});
+
+  Step.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        stationId = json['station_id'],
+        imagePath = json['image_path'],
+        floor = json['floor'],
+        lat = json['lat'],
+        lng = json['lng'],
+        name = json['name'],
+        category = json['category'],
+        description = json['description'],
+        level = json['level'],
+        createdAt = json['created_at'],
+        updatedAt = json['updated_at'];
+
+  Map<String, dynamic> toJson() => {
+        'station_id': stationId,
+        'image_path': imagePath,
+        'floor': floor,
+        'lat': lat,
+        'lng': lng,
+        'name': name,
+        'category': category,
+        'description': description,
+        'level': level,
+        'created_at': createdAt,
+        'updated_at': updatedAt,
+      };
+}

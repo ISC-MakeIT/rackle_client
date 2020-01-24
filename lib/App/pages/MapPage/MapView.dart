33f3a632c8cfb52353a6f769ea82c3c908212c60
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2020-01-24 20:04:06 +0900

    :sparkles:ADD MapView widget to MapView.dart
    ~

---
+++ lib/App/pages/MapPage/MapView.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapView extends StatefulWidget {
+  @override
+  State<MapView> createState() => MapViewState();
+}
+
+class MapViewState extends State<MapView> {
+  // Completer<GoogleMapController> _controller = Completer();
+  // final List<Marker> markers = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: GoogleMap(
+        mapType: MapType.normal,
+        // initialCameraPosition:
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
+        // onMapCreated: (GoogleMapController controller) {
+        //   _controller.complete(controller);
+        // },
+        indoorViewEnabled: true,
+        // markers: Set.from(markers),
+      ),
+    );
+  }
+}

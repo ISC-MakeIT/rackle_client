8c44d804b00ca9875c0406326dfa4ee56e839464
56miyano <bjgm1290561@gn.iwasaki.ac.jp>
2020-01-25 17:53:26 +0900

    add NotificationPage.dart SingleChildScrollView

--- lib/App/pages/NotificationsPage/NotificationsPage.dart
+++ lib/App/pages/NotificationsPage/NotificationsPage.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:rackle_client/App/pages/NotificationsPage/Newslist.dart';
+import 'package:rackle_client/App/pages/NotificationsPage/NotificationsHeading.dart';
+import 'package:rackle_client/App/pages/pages.dart';
 
 class NotificationsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('NotificationsPage'),
+    return SingleChildScrollView(
+      child: Container(
+        child: Column(
+          children: <Widget>[
+            NotificationsHeading(),
+            Newslist(),
+          ],
+        ),
+      ),
     );
   }
 }

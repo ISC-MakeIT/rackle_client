e5e708f588d268bc4ad8f32f6e399352ed2d8de7
isshun226 <bjsi1290346@gn.iwasaki.ac.jp>
2020-01-26 19:02:32 +0900

    Wrote AppBar Theme

--- lib/theme/rackleTheme.dart
+++ lib/theme/rackleTheme.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
 
 ThemeData rackleTheme = ThemeData(
+  appBarTheme: AppBarTheme(
+    textTheme: TextTheme(
+      title: TextStyle(
+        fontSize: 22.62,
+        color: Color(0XFF53514B),
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+    iconTheme: IconThemeData(
+      color: Color(0XFF53514B),
+    ),
+    elevation: 0,
+  ),
+  scaffoldBackgroundColor: Color(0XFFFFFFFF),
   textTheme: TextTheme(
     button: TextStyle(
       color: Color(0xFF53514B),

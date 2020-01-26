b3c703277afdad4f523ac1575584d2f2ae1e9880
isshun226 <bjsi1290346@gn.iwasaki.ac.jp>
2020-01-26 19:01:24 +0900

    [WiP]Writing MapView

--- lib/App/pages/Maps/MapPage/MapPage.dart
+++ lib/App/pages/Maps/MapPage/MapPage.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
+import '../MapView/MapView.dart';
+
 class MapPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('MapPage'),
+    return MapView(
+      initialCameraPosition: CameraPosition(
+        target: LatLng(35.4655618, 139.6220429),
+        zoom: 18
+      ),
     );
   }
 }

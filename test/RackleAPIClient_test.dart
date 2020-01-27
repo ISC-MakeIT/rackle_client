599ee7840542232380fd7938676ef77fca323684
isshun226 <bjsi1290346@gn.iwasaki.ac.jp>
2020-01-27 09:53:04 +0900

    Fixed test for APIClient

--- test/RackleAPIClient_test.dart
+++ test/RackleAPIClient_test.dart
@@ -1,26 +1,32 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'dart:io';
+import 'dart:convert';
 
 import 'package:rackle_client/API/RackleAPIClient.dart';
 
 void main() {
   final env = Platform.environment;
-  String message = 'environment values isn`t set';
-  dynamic shouldSkip = env['URL'] == null ? message : false;
+  String skipMessage = 'environment values isn`t set';
+  dynamic shouldSkip = env['URL'] == null ? skipMessage : false;
   test('APIClient request method', () async {
     final api = RackleAPIClient(
       url: env['URL'],
     );
 
-    final resp = await api.getRequest(path: env['API_PATH']);
+    final resp = await api.getRequest(path: env['API_PATH'], queryParameters: {
+      'lat': '20.2531',
+      'lng': '122.5557',
+    });
     final String body = resp.body;
-    print(body);
-
-    /// 正しいResponseがに対して存在する値を比較した時のテスト
-    /// expect(body[key], equal([value]));
-    /// 正しいResponseに対して存在しない値を比較した時のテスト
-    /// expect(body[key], equal([wrong_value]));
-    /// 間違ったResponseに対してランダムな値を比較した時のテスト
-    /// expect(body[wrong_key], equal([any_value]));
+    final parsed = json.decode(body);
+    expect(parsed['currentStation'], equals({
+      'id': 3,
+      'name': '小田原駅',
+      'latlng': 'POINT (139.155393 35.256445)',
+      'created_at': '2020-01-26T11:23:17.389Z',
+      'updated_at': '2020-01-26T11:23:17.389Z',
+      'lat': 35.256445,
+      'lng': 139.155393
+    }));
   }, skip: shouldSkip);
 }

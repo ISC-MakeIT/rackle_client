1165f37e23f28b0f10389c8fb56e8a682f4117b1
isshun226 <bjsi1290346@gn.iwasaki.ac.jp>
2020-01-27 14:06:58 +0900

    Added method to load env file

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 import 'App/SplashImage.dart';
 import 'App/pages/pages.dart';
 import 'theme/rackleTheme.dart';
 
-void main() => runApp(SplashImage(
-  theme: rackleTheme,
-  after: App(
-    pageOfIndex0: MapPage(),
-    pageOfIndex1: PinsSummaryPage(),
-    pageOfIndex2: NotificationsPage(),
-    pageOfIndex3: ConfigurationPage(),
-  ),
-));
+Future main() async {
+  await DotEnv().load('.env');
+
+  runApp(
+    SplashImage(
+      theme: rackleTheme,
+      after: App(
+        pageOfIndex0: MapPage(),
+        pageOfIndex1: PinsSummaryPage(),
+        pageOfIndex2: NotificationsPage(),
+        pageOfIndex3: ConfigurationPage(),
+      ),
+    ),
+  );
+}

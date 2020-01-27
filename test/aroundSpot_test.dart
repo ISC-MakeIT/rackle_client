a638970b9a28e7c7799e1b46267e151af4dca843
isshun226 <bjsi1290346@gn.iwasaki.ac.jp>
2020-01-27 09:52:42 +0900

    [WiP]

--- test/aroundSpot_test.dart
+++ test/aroundSpot_test.dart
@@ -1,91 +1,34 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'dart:convert';
+import 'dart:io';
 
+import 'package:rackle_client/API/RackleAPIClient.dart';
 import 'package:rackle_client/models/models.dart';
 
 void main() {
+  final env = Platform.environment;
+  String skipMessage = 'environment values isn`t set';
+  dynamic shouldSkip = env['URL'] == null ? skipMessage : false;
   test(
     'aroundSpot model test',
-    () {
-      var jsonString = '''
-{
-    "aroundSpot": {
-        "elevetor": [
-            {
-                "id": 1,
-                "station_id": 2,
-                "image_path": "hoge",
-                "floor": 1,
-                "lat": 35.465786,
-                "lng": 139.622313,
-                "name": "hoge",
-                "category": "elevator",
-                "description": "hoge",
-                "size": 1,
-                "from": 1,
-                "to": 4,
-                "created_at": "2020-01-18T14:26:38.000Z",
-                "updated_at": "2020-01-18T14:26:38.000Z"
-            }
-        ],
-        "toilet": [
-            {
-                "id": 2,
-                "station_id": 2,
-                "image_path": "",
-                "floor": 2,
-                "lat": 35.465786,
-                "lng": 139.622313,
-                "name": "fuga",
-                "category": "toilet",
-                "description": "255字以上になることないよね？",
-                "is_multipurpose": "true",
-                "created_at": "2020-01-18T14:26:38.000Z",
-                "updated_at": "2020-01-18T14:26:38.000Z"
-            }
-        ],
-        "slope": [
-            {
-                "id": 3,
-                "station_id": 2,
-                "image_path": "",
-                "floor": 3,
-                "lat": 35.465786,
-                "lng": 139.622313,
-                "name": "uwaan",
-                "category": "slope",
-                "description": "",
-                "level": 1,
-                "created_at": "2020-01-18T14:26:38.000Z",
-                "updated_at": "2020-01-18T14:26:38.000Z"
-            }
-        ],
-        "step": [
-            {
-                "id": 4,
-                "station_id": 2,
-                "image_path": "",
-                "floor": 3,
-                "lat": 35.465786,
-                "lng": 139.622313,
-                "name": "uwaan",
-                "category": "step",
-                "description": "",
-                "level": 1,
-                "created_at": "2020-01-18T14:26:38.000Z",
-                "updated_at": "2020-01-18T14:26:38.000Z"
-            }
-        ]
-    }
-}
-    ''';
+    () async {
+      final api = RackleAPIClient(
+        url: env['URL'],
+      );
 
-      final parsed = json.decode(jsonString);
-      final aroundSpot = new AroundSpot.fromJson(parsed);
-      expect(aroundSpot.elevetor[0].id, 1);
-      expect(aroundSpot.toilet[0].id, 2);
-      expect(aroundSpot.slope[0].id, 3);
-      expect(aroundSpot.step[0].id, 4);
+      final resp =
+          await api.getRequest(path: env['API_PATH'], queryParameters: {
+        'lat': '20.2531',
+        'lng': '122.5557',
+      });
+      final String body = resp.body;
+      final parsed = json.decode(body);
+      final aroundSpot = AroundSpot.fromJson(parsed);
+      expect(aroundSpot.elevetor[0].id, equals('1'));
+      expect(aroundSpot.toilet[0].id, equals('2'));
+      expect(aroundSpot.slope[0].id, equals('3'));
+      expect(aroundSpot.step[0].id, equals('4'));
     },
+    skip: shouldSkip,
   );
 }

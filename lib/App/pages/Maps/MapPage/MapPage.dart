9645dbbf401c8b56c5519efebabe5a85c20d2ac0
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2020-01-28 11:18:01 +0900

    :sparkles:ADD summaryPanel to MapPage
    ~

--- lib/App/pages/Maps/MapPage/MapPage.dart
+++ lib/App/pages/Maps/MapPage/MapPage.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
 import '../MapView/MapView.dart';
+import './components/summaryPanel.dart';
 
 class MapPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MapView(
-      initialCameraPosition: CameraPosition(
-        target: LatLng(35.4655618, 139.6220429),
-        zoom: 18
+    return SummaryPanel(
+      body: MapView(
+        initialCameraPosition:
+            CameraPosition(target: LatLng(35.4655618, 139.6220429), zoom: 18),
       ),
     );
   }

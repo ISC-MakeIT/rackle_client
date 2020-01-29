3d3191da21c31f0653700bb6100fa056a6cf6fb0
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2020-01-29 15:53:01 +0900

    :sparkles:ADD station list view
    ~

--- lib/App/pages/Maps/MapPage/components/selectStation.dart
+++ lib/App/pages/Maps/MapPage/components/selectStation.dart
@@ -15,9 +15,9 @@ class SelectStation extends StatelessWidget {
             child: Text(
               "どの駅をご利用ですか？",
               style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
                 color: Color(0xff53514B),
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
               ),
             ),
           ),
@@ -31,6 +31,53 @@ class SelectStation extends StatelessWidget {
             shape: RoundedRectangleBorder(
               borderRadius: new BorderRadius.circular(16.0),
             ),
+            child: Text(
+              "横浜駅を利用する",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+            ),
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.only(
+            top: 36,
+            bottom: 6,
+          ),
+          child: Align(
+            alignment: Alignment.topLeft,
+            child: Text(
+              "対応済み駅マップ",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Color(0xff53514B),
+              ),
+            ),
+          ),
+        ),
+        // Expanded(
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height * .55,
+          child: ListView.builder(
+            shrinkWrap: true,
+            itemCount: 10,
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                leading: Icon(Icons.business),
+                title: Text(
+                  "横浜駅",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                subtitle: Text("対応状況"),
+              );
+            },
           ),
         ),
       ],

9231a427b1cb1bddac9c38c72984826d33e8bf1e
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2020-01-31 11:31:19 +0900

    :recycle:DEL _panel widget
    ~

--- lib/App/pages/Maps/MapPage/components/summaryPanel.dart
+++ lib/App/pages/Maps/MapPage/components/summaryPanel.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
-import './selectStation.dart';
+import './pinSummary.dart';
 
 class SummaryPanel extends StatelessWidget {
   final body;
@@ -12,46 +12,17 @@ class SummaryPanel extends StatelessWidget {
     final _panelHeightOpen = MediaQuery.of(context).size.height * .80;
 
     return SlidingUpPanel(
-      panel: _panel(),
+      padding: EdgeInsets.only(
+        left: 22,
+        right: 22,
+      ),
+      panelBuilder: (ScrollController sc) => PinSummary(sc),
       body: body,
       maxHeight: _panelHeightOpen,
       backdropEnabled: true,
       borderRadius: BorderRadius.only(
         topLeft: Radius.circular(15),
         topRight: Radius.circular(15),
-      ),
-    );
-  }
-
-  Widget _panel() {
-    return Container(
-      padding: EdgeInsets.only(
-        left: 22,
-        right: 22,
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          SizedBox(
-            height: 12.0,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                width: 30,
-                height: 5,
-                decoration: BoxDecoration(
-                  color: Colors.grey[300],
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(12.0),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          SelectStation(),
-        ],
       ),
     );
   }

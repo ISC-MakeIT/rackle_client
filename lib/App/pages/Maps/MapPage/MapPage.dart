69d2c171d03af4b7e652e4ef698fe491d6671ed9
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2020-02-04 11:23:06 +0900

    :truck:MOVE panel components
    ~

--- lib/App/pages/Maps/MapPage/MapPage.dart
+++ lib/App/pages/Maps/MapPage/MapPage.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 import '../MapView/MapView.dart';
-import './components/summaryPanel.dart';
+import '../Panels/panel.dart';
 
 class MapPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SummaryPanel(
+    return Panel(
       body: MapView(
         initialCameraPosition:
             CameraPosition(target: LatLng(35.4655618, 139.6220429), zoom: 18),

819fbfa3fa1b10147cf4b567c830069d04e16460
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2020-02-13 19:33:03 +0900

    :fire:DEL App widget code
    ~

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'App/SplashImage.dart';
-import 'App/pages/pages.dart';
-import 'theme/rackleTheme.dart';
 
 Future main() async {
   await DotEnv().load('.env');
 
   runApp(
-    SplashImage(
-      theme: rackleTheme,
-      after: App(
-        pageOfIndex0: MapPage(),
-        pageOfIndex1: PinsSummaryPage(),
-        pageOfIndex2: NotificationsPage(),
-        pageOfIndex3: ConfigurationPage(),
-      ),
-    ),
+    SplashImage(),
   );
 }

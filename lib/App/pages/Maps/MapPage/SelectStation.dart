690ffe1f8b7c5a82fbfe3b8ebf347377392e1eba
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2020-02-13 15:51:03 +0900

    :construction:[WIP]ADD MapPage navigation
    ~

--- lib/App/pages/Maps/MapPage/SelectStation.dart
+++ lib/App/pages/Maps/MapPage/SelectStation.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
 class SelectStation extends StatelessWidget {
+  final onPush;
+  SelectStation({this.onPush});
+
   Widget build(BuildContext context) {
-    return null;
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => onPush(0, 0),
+      ),
+    );
   }
 }

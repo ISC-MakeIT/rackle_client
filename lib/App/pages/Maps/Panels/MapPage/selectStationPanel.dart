b7fdb1d32084d2571841367daac4622719ffb27b
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2020-02-15 12:22:27 +0900

    :recycle:ADD singlechildscrollview to SelectStationPanel widget
    ~

--- lib/App/pages/Maps/Panels/MapPage/selectStationPanel.dart
+++ lib/App/pages/Maps/Panels/MapPage/selectStationPanel.dart
@@ -2,88 +2,96 @@ import 'package:flutter/material.dart';
 
 class SelectStationPanel extends StatelessWidget {
   final sc;
-  SelectStationPanel({Key key, this.sc}) : super(key: key);
+  final onPush;
+  SelectStationPanel({Key key, this.sc, this.onPush}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-          margin: EdgeInsets.only(
-            top: 18.3,
-            bottom: 19,
-          ),
-          child: Align(
-            alignment: Alignment.topLeft,
-            child: Text(
-              "どの駅をご利用ですか？",
-              style: TextStyle(
-                color: Color(0xff53514B),
-                fontWeight: FontWeight.bold,
-                fontSize: 16,
-              ),
+    return SingleChildScrollView(
+      controller: sc,
+      child: Column(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(
+              top: 18.3,
+              bottom: 19,
             ),
-          ),
-        ),
-        Container(
-          width: MediaQuery.of(context).size.width,
-          height: 56,
-          child: RaisedButton(
-            onPressed: () {},
-            color: Color(0xff26BD95),
-            shape: RoundedRectangleBorder(
-              borderRadius: new BorderRadius.circular(16.0),
-            ),
-            child: Text(
-              "横浜駅を利用する",
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: 16,
-              ),
-            ),
-          ),
-        ),
-        Container(
-          margin: EdgeInsets.only(
-            top: 36,
-            bottom: 6,
-          ),
-          child: Align(
-            alignment: Alignment.topLeft,
-            child: Text(
-              "対応済み駅マップ",
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                color: Color(0xff53514B),
-              ),
-            ),
-          ),
-        ),
-        //TODO:need refactoring
-        Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height * .55,
-          child: ListView.builder(
-            shrinkWrap: true,
-            itemCount: 10,
-            itemBuilder: (BuildContext context, int index) {
-              return ListTile(
-                leading: Icon(Icons.business),
-                title: Text(
-                  "横浜駅",
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                "どの駅をご利用ですか？",
+                style: TextStyle(
+                  color: Color(0xff53514B),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
                 ),
-                subtitle: Text("対応状況"),
-              );
-            },
+              ),
+            ),
           ),
-        ),
-      ],
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 56,
+            child: RaisedButton(
+              onPressed: () => onPush(),
+              color: Color(0xff26BD95),
+              shape: RoundedRectangleBorder(
+                borderRadius: new BorderRadius.circular(16.0),
+              ),
+              child: Text(
+                "横浜駅を利用する",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(
+              top: 36,
+              bottom: 6,
+            ),
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                "対応済み駅マップ",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xff53514B),
+                ),
+              ),
+            ),
+          ),
+          //TODO:need refactoring
+          SingleChildScrollView(
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: 10,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  leading: Icon(Icons.business),
+                  onTap: () => onPush(),
+                  title: Text(
+                    "横浜駅",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  subtitle: Row(
+                    children: <Widget>[
+                      Text("対応状況"),
+                      // アイコン配置
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }

9175129cbe002c2e216262b86d16d42df354235d
isshun226 <bjsi1290346@gn.iwasaki.ac.jp>
2020-02-24 20:31:34 +0900

    remove _env and directly specified url

--- lib/API/RackleClient.dart
+++ lib/API/RackleClient.dart
@@ -2,48 +2,20 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:http/http.dart' as http;
-import 'package:meta/meta.dart';
 
-import 'package:rackle_client/Models/models.dart';
+class RackleClient {
+  String _url = '18.177.172.232:3000';
 
-final _env = Platform.environment;
-
-class _RackleClient {
-  String url;
-
-  _RackleClient({
-    @required this.url,
-  });
-
-  Future<List<Stations>> getStations() async {
-    List list = await get(path: 'stations');
-    return list.map((json) => Stations.fromJson(json)).toList();
-  }
-
-  Future<List<Spot>> getSpot() async {
-    List list = await get(path: 'stations');
-    return list.map((json) => Spot.fromJson(json)).toList();
-  }
-
-  Future<List<News>> getNews() async {
-    List list = await get(path: 'news');
-    return list.map((json) => News.fromJson(json)).toList();
-  }
-
-  Future<List<Platforms>> getPlatforms() async {
-    List list = await get(path: 'st_platforms');
-    return list.map((json) => Platforms.fromJson(json)).toList();
-  }
-
-  Future<dynamic> get({
-    String path = '',
+  Future<List<dynamic>> get(
+    String path, {
     Map<String, String> queryParameters,
   }) async {
-    var _json;
+    List<dynamic> _json;
+
     try {
       var resp = await http.get(
         _buildUri(
-          path: path == '' ? _env['API_PATH'] : '/api/v1/$path',
+          path: '/api/v1/$path',
           queryParameters: queryParameters,
         ),
         headers: {
@@ -54,14 +26,16 @@ class _RackleClient {
     } catch (e) {
       throw e;
     }
+
     return _json;
   }
 
   Uri _buildUri({
     String path = '',
     Map<String, String> queryParameters,
-  }) =>
-      Uri.http(this.url, path, queryParameters);
+  }) {
+    return Uri.http(this._url, path, queryParameters);
+  }
 }
 
-final api = _RackleClient(url: _env['URL']);
+final api = RackleClient();

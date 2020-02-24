9e4be214914313f1f8620fd741760b416eaf0c83
isshun226 <bjsi1290346@gn.iwasaki.ac.jp>
2020-02-24 21:11:08 +0900

    refactored

--- lib/Models/NewsModel.dart
+++ lib/Models/NewsModel.dart
@@ -2,11 +2,14 @@ import 'package:rackle_client/API/RackleClient.dart' show api;
 import 'package:rackle_client/Entitys/entitys.dart';
 
 class NewsModel {
-  Future<List<News>> get value => fetchData();
-
-  Future<List<News>> fetchData() async {
-    var jsonList = await api.get('news');
-
+  Future<List<News>> get value async {
+    final jsonList = await fetchData();
     return jsonList.map((json) => News.fromJson(json)).toList();
+  }
+
+  Future<List<dynamic>> fetchData() async {
+    final List<dynamic> jsonList = await api.get('news');
+
+    return jsonList;
   }
 }
